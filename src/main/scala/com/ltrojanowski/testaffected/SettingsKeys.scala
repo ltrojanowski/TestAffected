80604d9779092e089c7933de02697bf78a01ea04
ltrojanowski <ltrojanowski@virtuslab.com>
2020-01-24 15:48:48 +0100

    fix formatting

--- src/main/scala/com/ltrojanowski/testaffected/SettingsKeys.scala
+++ src/main/scala/com/ltrojanowski/testaffected/SettingsKeys.scala
@@ -8,6 +8,9 @@ trait SettingsKeys {
     SettingKey[Seq[String]]("ignored-files-or-dirs", "Specify relative paths of files or directories to exclude")
 
   lazy val diffAffectedProjects =
-    InputKey[(ResolvedProject, Set[ResolvedProject])]("diffAffectedProjects", "Returns the set of projects affected by the git diff")
+    InputKey[(ResolvedProject, Set[ResolvedProject])](
+      "diffAffectedProjects",
+      "Returns the set of projects affected by the git diff"
+    )
 
 }
