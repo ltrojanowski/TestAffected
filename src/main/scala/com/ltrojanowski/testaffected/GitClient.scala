dbe0d7e875cea3e0457a8b19df1dfd45c445fcc7
ltrojanowski <ltrojanowski@virtuslab.com>
2020-01-31 14:50:29 +0100

    update git client to handle old git versions

--- src/main/scala/com/ltrojanowski/testaffected/GitClient.scala
+++ src/main/scala/com/ltrojanowski/testaffected/GitClient.scala
@@ -50,28 +50,57 @@ class GitClientImpl(
 
   import GitClientImpl._
   import commandRunner._
+  import scala.util.matching._
 
-  override def findChangedFilesSince(sha: String, top: String, includeUncommitted: Boolean): List[String] = {
-    val pathToGitRepo = PATH_TO_GIT_REPO.runCommand().headOption
-    val excludeSuffix = ignoredFilesOrDirs
-      .flatMap(ignoredFileOrDir => pathToGitRepo.map(path => s":(exclude)$path$ignoredFileOrDir"))
+  private def getGitVersion() = {
+    GIT_VERSION.runCommand().headOption.flatMap(_.split(" ").takeRight(1).headOption)
+  }
+
+  private def logIgnoredFilesOrDirs(ignoredFilesOrDirs: Seq[String]) = {
     if (ignoredFilesOrDirs.isEmpty) {
       logger.info(s"Git diff will not exclude any files")
     } else {
       logger.info(s"Git diff will exclude the following files:\n${ignoredFilesOrDirs.mkString(" - ", "\n - ", "")}")
     }
-    val changedFiles = (
-      if (includeUncommitted) {
-        s"$CHANGED_FILES_CMD_PREFIX $sha${excludeSuffix.mkString(" -- . ", " ", "")}"
-      } else {
-        s"$CHANGED_FILES_CMD_PREFIX $top $sha${excludeSuffix.mkString(" -- . ", " ", "")}"
-      }
-    ).runCommand()
+  }
+
+  private def logChangedFiles(changedFiles: Seq[String]) = {
     if (changedFiles.isEmpty) {
       logger.info("Git diff didn't find any changed files")
     } else {
       logger.info(s"Git diff found the following files changed:\n${changedFiles.mkString(" - ", "\n - ", "")}")
     }
+  }
+
+  override def findChangedFilesSince(sha: String, top: String, includeUncommitted: Boolean): List[String] = {
+    val pathToGitRepo = PATH_TO_GIT_REPO.runCommand().headOption
+    val excludeSuffix = ignoredFilesOrDirs
+      .flatMap(ignoredFileOrDir => pathToGitRepo.map(path => s":(exclude)$path$ignoredFileOrDir"))
+    logIgnoredFilesOrDirs(ignoredFilesOrDirs)
+    val changedFiles = if (getGitVersion().exists(_ >= "1.9.5")) {
+      (
+        if (includeUncommitted) {
+          s"$CHANGED_FILES_CMD_PREFIX $sha${excludeSuffix.mkString(" -- . ", " ", "")}"
+        } else {
+          s"$CHANGED_FILES_CMD_PREFIX $top $sha${excludeSuffix.mkString(" -- . ", " ", "")}"
+        }
+      ).runCommand()
+    } else {
+      (
+        if (includeUncommitted) {
+          s"$CHANGED_FILES_CMD_PREFIX $sha"
+        } else {
+          s"$CHANGED_FILES_CMD_PREFIX $top $sha"
+        }
+      ).runCommand()
+        .filterNot(
+          unfilteredFilePath =>
+            ignoredFilesOrDirs.exists(
+              ignoredFile => new Regex(ignoredFile).findFirstIn(unfilteredFilePath).isDefined
+            )
+        )
+    }
+    logChangedFiles(changedFiles)
     changedFiles
       .flatMap(relativePath => pathToGitRepo.map(_ + File.separator + relativePath))
   }
@@ -118,4 +147,5 @@ object GitClientImpl {
   val BRANCHING_FROM_TARGET_PREFIX = s"git merge-base"
   val CHANGED_FILES_CMD_PREFIX     = "git diff --name-only"
   val HEAD_OF_BRANCH_PREFIX        = "git log -1 --oneline"
+  val GIT_VERSION                  = "git --version"
 }
